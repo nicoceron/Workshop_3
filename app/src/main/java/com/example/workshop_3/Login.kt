b261446d4d2bb7e6bb4a95fe834eebdad6f7ab57
nicoceron <nicocerond@gmail.com>
2024-04-30 14:34:47 -0500

    Login & Firebase connection

--- app/src/main/java/com/example/workshop_3/Login.kt
+++ app/src/main/java/com/example/workshop_3/Login.kt
@@ -1,20 +1,28 @@
 package com.example.workshop_3
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.service.voice.VoiceInteractionSession.ActivityId
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.workshop_3.databinding.ActivityLoginBinding
 import com.example.workshop_3.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
 class Login : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -22,7 +30,58 @@ class Login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
+        auth = FirebaseAuth.getInstance()
     }
+
+    override fun onStart() {
+        super.onStart()
+        updateUI(auth.currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null) {
+            val i = Intent(this, MainActivity::class.java)
+            i.putExtra("email", currentUser.email.toString())
+            startActivity(i)
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        if (isValidEmail(email) && password != null) {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    updateUI(auth.currentUser)
+                } else {
+                    val message = it.exception!!.message
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                    Log.w(TAG, "signInWithEmailAndPassword", it.exception)
+                    binding.email.text?.clear()
+                    binding.password.text?.clear()
+                }
+            }
+        }
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        var valid = false
+        if (email.isEmpty()) {
+            binding.email.setError("Required!")
+        } else if (!isValidEmail(email)) {
+            binding.email.setError("Invalid email address")
+        } else if (password.isEmpty()) {
+            binding.password.setError("Required")
+        } else if (password.length < 6) {
+            binding.password.setError("Password should be at least 6 characters long")
+
+        } else {
+            valid = true
+        }
+        return valid
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\$"
+        return email.matches(emailRegex.toRegex())
+    }
+
 }
